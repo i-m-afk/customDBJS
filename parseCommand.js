74c57a5e2cebebf9b20e43ab20bd7fb6750a60ce
i-m-afk <rishavkumar208@gmail.com>
2023-08-06 19:07:53 +0530

    refactor

--- parseCommand.js
+++ parseCommand.js
@@ -1,10 +1,16 @@
 const parseInsertCommand = require("./parsers/insert");
 const parseSelectCommand = require("./parsers/select");
 const parseComment = require("./parsers/comment");
-const parsers = [parseInsertCommand, parseSelectCommand, parseComment];
+const parseWhereCommand = require("./parsers/where");
+const parseDeleteCommand = require("./parsers/delete");
+const InvalidCommandError = require("./errors/InvalidCommandError");
+
+const parsers = [parseInsertCommand, parseSelectCommand, parseComment, parseDeleteCommand];
 module.exports = async function parseCommand(commandString) {
   const command = parsers.map((parser) => parser(commandString)).find((command) => command != null);
 
   if (command == null) throw new InvalidCommandError(CommandString);
-  return await command.perform();
+
+  const whereCommand = parseWhereCommand(commandString);
+  return await command.performWhere(whereCommand);
 };
